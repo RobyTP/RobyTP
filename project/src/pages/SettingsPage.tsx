1a98a8f3e1e5d6828ac42f65b25f037f2cc3dd9e
Ritschll-Isaac <ritschll-isaac.prophete.1@ulaval.ca>
2025-04-30 14:46:22 -0400

    effacement total

--- project/src/pages/SettingsPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DashboardSettings from './dashboard/DashboardSettings';
-
-const SettingsPage: React.FC = () => {
-  return <DashboardSettings />;
-};
-
-export default SettingsPage;
